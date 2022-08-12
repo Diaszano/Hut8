94631d69055c01291b2970aa50e9ec1dfb2aa461
Diaszano <lucasdiiassantos@gmail.com>
2022-08-11 22:30:36 -0300

    ex_1 feito

--- Semana 03 - Lógica de Programação/Exercícios/ex_1.js
+++ Semana 03 - Lógica de Programação/Exercícios/ex_1.js
@@ -1,1 +1,33 @@
 // Primeiro exercício
+const soma = function (number_1,number_2) {
+    return number_1 + number_2;
+}
+
+const subtrai = function(number_1,number_2) {
+    return number_1 - number_2;
+}
+
+const divide = function(number_1,number_2) {
+    return number_1 / number_2;
+}
+
+const multiplica = function(number_1,number_2) {
+    return number_1 * number_2;
+}
+
+const operacoes = function(name,number_1,number_2){
+    switch(name){
+        case 'soma':
+            return soma(number_1,number_2);
+        case 'subtrai':
+            return subtrai(number_1,number_2);
+        case 'divide':
+            return divide(number_1,number_2);
+        case 'multiplica':
+            return divide(number_1,number_2);
+        default:
+            throw new Error(`O tipo "${name}" não é disponível!`);
+    }
+}
+
+console.log(`O valor da soma de 3 + 5 é: ${operacoes('soma',3,5)}`);
